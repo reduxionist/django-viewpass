db33e4c764b33f38a3f6970382aaa6b731a8a899
Joel Burton <joel@joelburton.com>
2015-12-22 01:01:30 -0500

    Fix license.

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ from setuptools import setup
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
 
-# allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
@@ -14,7 +13,7 @@ setup(
         version='1.0.1',
         packages=['viewpass'],
         include_package_data=True,
-        license='GPL',  # example license
+        license='GPL',
         description='A Django app to provide access to protected resources via URL tokens.',
         long_description=README,
         url='https://github.com/joelburton/viewpass.git',
@@ -25,10 +24,9 @@ setup(
             'Environment :: Web Environment',
             'Framework :: Django',
             'Intended Audience :: Developers',
-            'License :: OSI Approved :: GPL License',  # example license
+            'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
             'Operating System :: OS Independent',
             'Programming Language :: Python',
-            # Replace these appropriately if you are stuck on Python 2.
             'Programming Language :: Python :: 2',
             'Programming Language :: Python :: 2.7',
             'Programming Language :: Python :: 3',
